35355fd262189a1cbbdc64310a3d5dfc1fd0f0eb
Guilherme Augusto <guilherme.augst@gmail.com>
2024-10-11 19:49:40 -0300

    first commit

---
+++ tarefa/views.py
@@ -0,0 +1,33 @@
+from .models import Tarefa
+from .forms import TarefaForm
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'lista.html')
+
+# Listar tarefas
+class ListaTarefa(ListView):
+    model = Tarefa
+    template_name = 'lista.html'
+    context_object_name = 'tarefas'
+
+ # Nova Tarefa 
+class NovaTarefa(CreateView):
+    model = Tarefa
+    form_class = TarefaForm
+    template_name = 'nova.html'
+    success_url = '/lista'
+
+# Atulizar Tarefas
+class AtualizarTarefa(UpdateView):
+    model = Tarefa
+    form_class = TarefaForm
+    template_name = 'atualizar.html'
+    success_url  ='/lista'
+
+# Apagar tarefas atualizadas
+class DeletarTarefa(DeleteView):
+    model = Tarefa
+    template_name = 'deletar.html'
+    success_url = '/lista'
